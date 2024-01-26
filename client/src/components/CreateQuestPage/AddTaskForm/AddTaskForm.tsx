d0086830f4f2a49102c136439ba24c5f21357e1e
reina-duf <reina.dufrene@gmail.com>
2024-01-25 19:33:28 -0700

    move min tasks memo to left

--- client/src/components/CreateQuestPage/AddTaskForm/AddTaskForm.tsx
+++ client/src/components/CreateQuestPage/AddTaskForm/AddTaskForm.tsx
@@ -16,7 +16,7 @@ export default function AddTaskForm({addTask}: Props) {
   return(
        <form onSubmit={handleAddTask} className="flex flex-col justify-center text-center mt-2 relative">
         <label className="font-black uppercase tracking-wider mb-2">✅ Task Details ✅</label>
-        <span className="text-xs italic absolute font-semibold bottom-[4.5rem] right-1 text-white">*Min 3 tasks required</span>
+        <span className="text-xs italic absolute font-semibold bottom-[4.5rem] left-0 text-white">*Min. 3 tasks required</span>
         <textarea placeholder="Task details..." className="p-3 mb-4 h-36 rounded-md" name="description" value={description} onChange={(evt) => setDescription(evt.target.value)}></textarea>
         <button className="my-5 bg-black px-7 py-2 text-white rounded-sm text-sm uppercase tracking-widest w-1/2 mx-auto font-bold" type="submit">Add Task</button>
       </form>
