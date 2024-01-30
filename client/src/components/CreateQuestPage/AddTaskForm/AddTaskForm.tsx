bbde9a6e7d284ceff5476f1eb8821afbd41d6a74
reina-duf <reina.dufrene@gmail.com>
2024-01-29 21:49:03 -0700

    baby styling updates

--- client/src/components/CreateQuestPage/AddTaskForm/AddTaskForm.tsx
+++ client/src/components/CreateQuestPage/AddTaskForm/AddTaskForm.tsx
@@ -31,7 +31,7 @@ export default function AddTaskForm({addTask}: Props) {
        <form onSubmit={handleAddTask} className="flex flex-col justify-center text-center mt-2">
         <label className="font-black uppercase tracking-wider mb-2">4. Add tasks - at least 3!</label>
         <textarea placeholder="Task details..." className="p-3 mb-4 h-36 rounded-md" name="description" value={taskDetails.description} onChange={handleChange}></textarea>
-        <label className="font-black uppercase tracking-wider mb-2">5. OPTIONAL - add a hint for your task</label>
+        <label className="font-black uppercase tracking-wider mb-2">5. <span className="text-mq-purple">Optional</span> - add a hint for your task</label>
         <textarea placeholder="Hint details..." className="p-3 mb-4 h-36 rounded-md" name="hint" value={taskDetails.hint} onChange={handleChange}></textarea>
         <button className="my-5 bg-black px-7 py-2 text-white rounded-sm text-sm uppercase tracking-widest w-1/2 mx-auto font-bold" type="submit">Add Task</button>
       </form>
