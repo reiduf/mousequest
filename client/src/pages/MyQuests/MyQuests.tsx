48c29b86f44b8316af3359836fc9ec72d08a4290
reina-duf <reina.dufrene@gmail.com>
2024-01-31 19:54:47 -0700

    updates to datetime for styling porpoises

--- client/src/pages/MyQuests/MyQuests.tsx
+++ client/src/pages/MyQuests/MyQuests.tsx
@@ -21,11 +21,11 @@ export default function MyQuests() {
     getAcceptedQuests();
   }, [])
 
- 
+  const titleStyle = "text-xl lg:text-2xl font-black uppercase tracking-widest"
   return (
     <main className="mq-bg">
       <div className="flex justify-center">
-        <h2 className="mq-title">Active Quests ({activeQuests.length})</h2>
+        <h2 className={titleStyle}>Active Quests ({activeQuests.length})</h2>
       </div>
       <ActiveQuestList activeQuests={activeQuests} />
 
@@ -33,7 +33,7 @@ export default function MyQuests() {
         <svg className="w-6 h-6 stroke-black stroke-2" style={showCompletedList ? {transform: "rotate(90deg)", transition: "transform 0.35s"} : {transition: "transform 0.35s"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" >
           <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
         </svg>
-        <h2 className="mq-title text-center">
+        <h2 className={titleStyle}>
           Completed Quests ({completedQuests.length})
         </h2>
       </div>
