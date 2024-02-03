da3f7142f8cbc5edc06c60afabc718ef260ac45d
reina-duf <reina.dufrene@gmail.com>
2024-02-02 20:01:59 -0700

    updates styling for splash page

--- client/src/components/AuthPage/About/About.tsx
+++ client/src/components/AuthPage/About/About.tsx
@@ -3,20 +3,20 @@ import Faq from "../Faq/Faq"
 
 export default function About() {
   const faq1 = "MouseQuest is an immersive experience of community-crafted scavenger hunts throughout Disneyland and California Adventure. Vote for your favorites, discover quests tailored to your interests or your family’s, or contribute by creating your own quests for the community to savor!";
-  const faq2 = "You can find quests that community memebers created on the find tab. Find a quest that fits your style and click accept. You will then have your own copy of the quest that traks your progress!"
-  const faq3 = "Yes! There are several categories you can choose from, including Kid Friendly, Adult Friendly, Family Friendly, Easy, Hard, and many more!"
+  const faq2 = "You can find quests created by community memebers on the find tab. Find a quest that fits your style and click accept. You will then have your own copy of the quest that tracks your progress!"
+  const faq3 = "Yes! There are several categories you can choose from, including: Kid Friendly, Adult Friendly, Family Friendly, Easy, Hard, and many more!"
   return (
     <main className="h-[95vh] bg-mq-boring">
-      <div className=" bg-cover bg-[60%_0%] lg:h-[60%] h-1/3 lg:bg-center lg:p-10 p-4 flex items-end shadow-xl" style={{backgroundImage: `url('${mickeyUrl}')`}}>
+      <div className=" bg-cover bg-[60%_0%] lg:h-[60%] h-1/3 lg:bg-center lg:p-10 p-4 flex items-start lg:items-end shadow-xl" style={{backgroundImage: `url('${mickeyUrl}')`}}>
           <div className="2xl:w-1/3 xl:w-1/2 w-full h-fit bg-white/85 rounded-2xl backdrop-blur-sm text-center xl:p-8 p-3">
-            <p className="lg:text-2xl uppercase text-xs font-semibold xl:text-left mb-1 italic">✨ Disneyland Scavenger hunts ✨</p>
+            <p className="lg:text-2xl 2xl:text-xl uppercase text-xs font-semibold xl:text-left mb-1 italic">✨ Disneyland Scavenger hunts ✨</p>
             <p className="lg:text-7xl text-2xl font-black xl:text-left uppercase">powered by <span className="text-mq-purple">YOU!</span></p>
           </div>
       </div>
       <div className="h-[0.35rem] bg-gradient-to-r from-mq-purple to-mq-blue flex flex-col justify-end"></div>
-      <h1 className="text-center font-black uppercase tracking-wider text-3xl mt-16 lg:mb-6 mb-0">About MouseQuest / FAQ</h1>
+      <h1 className="text-center font-black uppercase tracking-wider lg:text-3xl text-xl mt-10 lg:mb-6 mb-0">About MouseQuest / FAQ</h1>
       <div className="bg-mq-boring h-[60%] p-4 w-full text-center">
-        <Faq title={"What is MouseQuest?"} desc={faq1}/>
+        <Faq title={"What is MQ?"} desc={faq1}/>
         <Faq title={"How does it work?"} desc={faq2}/>
         <Faq title={"Is it kid friendly?"} desc={faq3}/>
       </div>
--- client/src/components/AuthPage/Faq/Faq.tsx
+++ client/src/components/AuthPage/Faq/Faq.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Faq({title, desc}: Props) {
   const [showDesc, setShowDesc] = useState(false)
   return (
-    <div className="bg-white  rounded-xl lg:max-w-2xl border-mq-purple border-4 lg:inline-flex flex-col mx-3 xl:w-1/4 lg:w-1/3 w-full lg:my-0 my-4">
+    <div className="bg-white rounded-xl border-mq-purple border-4 lg:inline-flex flex-col lg:mx-3 lg:w-[30%] w-full lg:my-0 my-4">
       <div onClick={() => setShowDesc(!showDesc)} className="bg-mq-purple py-2 flex items-center lg:justify-center gap-3 px-2 cursor-pointer">
         <svg className="lg:w-6 lg:h-6 w-4 h-4 stroke-white stroke-2" style={showDesc ? {transform: "rotate(90deg)", transition: "transform 0.35s"} : {transition: "transform 0.35s"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" >
           <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
@@ -16,8 +16,8 @@ export default function Faq({title, desc}: Props) {
         <h1 className="font-black uppercase tracking-wide text-center xl:text-xl text-sm text-white">{title}</h1>
       </div>
       {showDesc &&
-      <div className="p-6">
-        <p className="mb-4 text-justify leading-7">{desc}</p>
+      <div className="xl:p-5 lg:p-3 p-5">
+        <p className="text-justify lg:text-sm xl:leading-6">{desc}</p>
       </div>
       }
     </div>
